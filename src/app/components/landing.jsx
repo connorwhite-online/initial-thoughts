7299d0ca530577da651b39042eeeb4d4227f77db
Connor White <connorwhitepdx@gmail.com>
2023-06-12 16:02:25 -0700

    strange margin fixed

--- src/app/components/landing.jsx
+++ src/app/components/landing.jsx
@@ -3,7 +3,7 @@ import styles from '../styles/landing.module.css';
 export default function Landing(){
     return (
         <div className={styles.landing}>
-            <div className={styles.titleContainer}><h1 className={styles.title}>INITIAL THOUGHTS</h1></div>
+            <div className={styles.titleContainer}><div className={styles.title}>INITIAL THOUGHTS</div></div>
         </div>
     )
 }

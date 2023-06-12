e9a339c2d889c6850dc571c63e28c26cf4d27e7a
Connor White <connorwhitepdx@gmail.com>
2023-06-12 13:27:22 -0700

    templating trimmed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Helvetica } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const helvetica = Helvetica({ subsets: ['regular'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={helvetica.className}>{children}</body>
     </html>
   )
 }
